926779122a5745c9a077a11a2e032195df624558
DongGeun974 <zxcasd3004@naver.com>
2022-08-03 18:50:45 +0900

    fix make dump

--- data/db_init.py
+++ data/db_init.py
@@ -6,4 +6,38 @@
 @Author ： Hwang
 @Date ：2022-08-03 오전 10:52 
 '''
+import json
+import re
 
+import pymysql
+
+f=open("db")
+pw = f.readline()
+f.close()
+
+with open("review.json",'r',encoding="UTF-8") as file:
+    reviews = json.load(file)
+
+
+db = pymysql.connect(
+            host="localhost",
+            port=3306,
+            user='root',
+            password=pw,
+            db='kurly', charset='utf8', autocommit=True  # 실행결과확정
+        )
+
+cursor = db.cursor()
+sql = """INSERT INTO review (`star`, `comment`, `date`) VALUES (%s, %s, %s)"""
+
+def romoveEmoji_ascii(string):
+    only_BMP_pattern = re.compile("["
+                                  u"\U00010000-\U0010FFFF"  # BMP characters 이외
+                                  "]+", flags=re.UNICODE)
+    return (only_BMP_pattern.sub(r'', string))  # BMP characters만
+
+
+
+reviews_query = [ (reviews[i]['star'], reviews[i]['comment'], reviews[i]['date']) for i in range(len(reviews))]
+
+cursor.executemany(sql, reviews_query)
--- data/make_review.py
+++ data/make_review.py
@@ -8,6 +8,7 @@
 '''
 import json
 import random
+import re
 import time
 
 def str_time_prop(start, end, format, prop):
@@ -19,6 +20,12 @@ def str_time_prop(start, end, format, prop):
 def random_date(start, end, prop):
     return str_time_prop(start, end, '%Y-%m-%d %H:%M:%S', prop)
 
+def romoveEmoji(string):
+    only_BMP_pattern = re.compile("["
+                                  u"\U00010000-\U0010FFFF"  # BMP characters 이외
+                                  "]+", flags=re.UNICODE)
+    return (only_BMP_pattern.sub(r'', string))  # BMP characters만
+
 f = open("ratings_total.txt",'r', encoding="UTF-8")
 review = []
 
@@ -26,9 +33,12 @@ lines = f.readlines()
 for l in lines:
     l = l.strip()
     l = l.split("\t")
+
+
+
     review.append(
         {"star" : l[0],
-         "comment" : l[1],
+         "comment" : romoveEmoji(l[1]),
          "date" : random_date("2021-1-1 23:59:59", "2022-1-1 23:59:59", random.random())})
 
 with open('review.json','w',encoding='utf-8') as f:
